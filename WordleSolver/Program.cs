1d1fa16be3fda41766ba7a4716311c31a67790f4
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2023-02-16 17:50:01 +0000

    initial

---
+++ WordleSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordleSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
