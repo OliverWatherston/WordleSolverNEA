0b386218c1fdd20da5a7b23b2247e38e7d2dca07
Oliver Watherston <128710806+OliverWatherston@users.noreply.github.com>
2023-03-23 19:49:25 +0000

    feat: added wordle solver

---
+++ WordleSolver/Guess.cs
@@ -0,0 +1,14 @@
+﻿namespace WordleSolver
+{
+    public struct Guess
+    {
+        public string Word { get; set; }
+        public string Response { get; set; }
+
+        public Guess(string word, string response)
+        {
+            Word = word;
+            Response = response;
+        }
+    }
+}
--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace WordleSolver
 {
     internal class Program
     {
+        private static Solver _solver = new Solver();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wordle Solver.\nWord match usage described below.\n\nGreen: G\nYellow: Y\nGrey: X\n\n");
+            Console.Title = "Wordle Solver";
+
+            SolveWordle();
+        }
+
+        static void SolveWordle()
+        {
+            string guess = _solver.MakeGuess();
+            Console.WriteLine($"Guess : {guess.ToUpper()}");
+            
+            Console.Write("Response: ");
+
+            string wordMatch = Console.ReadLine();
+            if (wordMatch == null) // guard case for checking that a value does exist, only checks whether a value is inputted
+            {
+                Console.WriteLine("Please enter response from Wordle.");
+                SolveWordle();
+                return; // return to avoid executing the code below
+            }
+
+            wordMatch = wordMatch.ToUpper();
+
+            if (!Regex.IsMatch(wordMatch, @"\b[GYX]{5}\b"))
+            {
+                Console.WriteLine("You did not enter a valid response!");
+                SolveWordle();
+                return;
+            }
+
+            Guess guessStruct = new Guess(guess, wordMatch);
+            
+            _solver.RemoveWord(guess);
+            
+            _solver.UpdateMasks(guessStruct);
+            
+            _solver.FilterWordList();
+            
+            _solver.UpdateMaskWithRemainingWords();
+
+            SolveWordle();
         }
     }
 }
